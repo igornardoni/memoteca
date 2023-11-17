f745dc2affc0678da5958e91dbcab1c3eaab095c
igornardoni <igornardonik@gmail.com>
2023-11-17 20:34:58 -0300

    update validators

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,12 +10,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
-
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
 
   formulario!: FormGroup;
 
@@ -27,22 +21,34 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulário reativo'],
-      autoria: [''],
+
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(4)
+      ])],
+
       modelo: ['modelo1']
     })
   }
 
   criarPensamento(): void {
-    this.service.createPensamentos(this.pensamento).subscribe(
-      () => {
-        this.router.navigate(['/listar-pensamentos']);
-      }
-    );
-  }
+    console.log(this.formulario.status);
+    if(this.formulario.valid) {
+      this.service.createPensamentos(this.formulario.value).subscribe(
+        () => {
+          this.router.navigate(['/listar-pensamentos'])
+        }
+      )
+    };
+  };
 
 
-  cancelarPensamento(): void {
+  cancelar(): void {
     this.router.navigate(['/listar-pensamentos'])
   }
 
